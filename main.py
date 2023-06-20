40460083616a7027c21f7b7f7125e9733f1276ef
Jai Bajaj <jaibajaj06@gmail.com>
2023-06-20 10:46:28 -0400

    finish logic

--- main.py
+++ main.py
@@ -9,14 +9,22 @@ for line in lines:
     if line != "\n":
         new = line.replace("\n", "")
         if len(new) == 5:
-            wordlist.append(new)
+            wordlist.append(new.lower())
 
 cant = input("What letters can you not use: ")
 must = input("What letters must be in the word: ")
 must_in_place = input("What letters are in the right place (ex: __a_b): ")
 
+
+regex = must_in_place.replace("_", ".")
+wordlist = list(filter(lambda word: re.match(regex, word) != None, wordlist))
+
 regex = ""
-filtered = filter(lambda word: re.match(regex, word) != None, wordlist)
-re.match(regex)
+for letter in must:
+    regex += f"(?=.*{letter})"
+wordlist = list(filter(lambda word: re.match(regex, word) != None, wordlist))
+
+regex = f"(?!.*[{cant}])"
+wordlist = list(filter(lambda word: re.match(regex, word) != None, wordlist))
 
 print(wordlist)
